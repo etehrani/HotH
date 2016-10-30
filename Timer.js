69bbb64c946646bbc4ccf0e0a0b1964db982a73d
etehrani <emilytehrani5@gmail.com>
2016-10-29 20:31:54 -0700

    Add files via upload

---
+++ Timer.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+
+var time : float = 10.0;
+
+function Update () {
+    timer -= Time.deltaTime;
+    if (time == 0)
+        // Do something
+}
+
+function OnGUI(){
+    GUI.Box(new Rect(10, 10, 50, 20), "Time: " + timer.ToString("0"));
+}
