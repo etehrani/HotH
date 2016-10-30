c6738207ef91e15d6ce07ac93419191523b8ff41
etehrani <emilytehrani5@gmail.com>
2016-10-29 18:00:56 -0700

    Update CollectibleCaught.cs

--- CollectibleCaught.cs
+++ CollectibleCaught.cs
@@ -4,7 +4,9 @@ using System.Collections;
 public class CollectibleCaught : MonoBehaviour {
 	void OnTriggerEnter(Collider other) {
 		if(other.gameObject.CompareTag("Player")) {
+				// Increase player score
 			Rigidbody otherRB = other.gameObject.GetComponent<Player>().incScore();
+				// Remove itself
 			this.gameObject.SetActive(false);
 		}
 	}
