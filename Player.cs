f5863e17c0487a2cf894ab05cb6e7cf7fe26b098
etehrani <emilytehrani5@gmail.com>
2016-10-29 18:01:27 -0700

    Update Player.cs

--- Player.cs
+++ Player.cs
@@ -1,15 +1,28 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class Player : MonoBehaviour {
-
-	// Use this for initialization
+	public int getScore() {
+		return mScore;
+	}
+	public int getLives() {
+		return mLives;
+	}
+	public void incScore() {
+		mScore += 100;
+	}
+	public void decLives() {
+		mLives--;
+	}
+	private int mScore;
+	private int mLives;
+		// Use this for initialization
 	void Start () {
-	
+		mScore = 0;
+		mLives = 5;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
 	}
 }
