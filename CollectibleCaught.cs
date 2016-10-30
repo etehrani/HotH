2b37cba9b7ddb756506f6932611e9b6a1532457c
etehrani <emilytehrani5@gmail.com>
2016-10-29 18:00:02 -0700

    Add files via upload

---
+++ CollectibleCaught.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollectibleCaught : MonoBehaviour {
+	void OnTriggerEnter(Collider other) {
+		if(other.gameObject.CompareTag("Player")) {
+			Rigidbody otherRB = other.gameObject.GetComponent<Player>().incScore();
+			this.gameObject.SetActive(false);
+		}
+	}
+}
